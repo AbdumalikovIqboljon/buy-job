409ef607da9f71a6ece007cdfe1e478858f0515e
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-06-15 15:02:01 +0500

    feat: add fonts and images, package of vue-router

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="max-w-340 w-full mx-auto bg-green-700">
+        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit quasi sint laboriosam.</h1>
+    </div>
+</template>
---
+++ src/router/routes.js
@@ -0,0 +1,8 @@
+import Home from "../pages/Home.vue";
+export const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+]
