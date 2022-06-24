842095ecc518276fa4fdcc6f02e8f128b6639a26
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-06-24 22:14:41 +0500

    feat: created login page logo

---
+++ src/pages/Login.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SiteLogo from "../components/svgs/SiteLogoDark.vue";
+</script>
+
+<template>
+<div class="bg-darkblue w-full flex items-center justify-center h-[100vh]">
+    <div class="flex flex-col items-center max-w-112.5 w-full bg-white px-18 py-14 rounded-lg">
+    <SiteLogo />
+</div>
+</div>
+</template>
