b2f0038d87db6b07c87196a605e1d8ac20e738a4
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-06-25 10:19:34 +0500

    feat: reusable footer has been completed

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,8 +1,21 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import TheFooterLogoGroup from "./TheFooterLogoGroup.vue";
+import BaseIcon from "./BaseIcon.vue";
+import BaseButton from "./BaseButton.vue";
 </script>
 
 <template>
-    <p>Способы оплаты</p>
-    
+  <div class="max-w-330 w-full mx-auto">
+    <div class="flex items-center">
+      <p class="mr-3.5">Способы оплаты</p>
+      <TheFooterLogoGroup class="flex items-center mr-auto"/>
+      <p>Права защищены и опасны</p>
+      <router-link :to="{ name: 'home' }" class="flex items-center mr-5 ml-15">
+        <BaseIcon name="ChatIcon" />
+        <p class="ml-3 text-lg">Поддержка</p>
+      </router-link>
+      <BaseButton type="warning">Разместить заказ</BaseButton>
+    </div>
+  </div>
 </template>
--- src/pages/Support.vue
+++ src/pages/Support.vue
@@ -8,9 +8,22 @@ import TheFooter from "@/components/TheFooter.vue";
     <TheHeader />
   </header>
   <main>
-  122
+21
   </main>
-  <footer class="bg-darkblue text-white">
+  <footer class="bg-darkblue text-white py-5">
     <TheFooter />
   </footer>
 </template>
+<style>
+html {
+  height: 100%;
+}
+#app {
+  min-height: 100vh !important;
+  display: flex !important;
+  flex-direction: column !important;
+}
+footer {
+  margin-top: auto;
+}
+</style>
