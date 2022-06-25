af2b61fb604a8b03ec8bd96aa9e8c0db816a1465
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-06-25 07:24:41 +0500

    fix: SVG files changed to .vue components to create reusable SVG

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,13 @@
 <script setup>
 import SiteLogo from "../components/svgs/SiteLogoDark.vue";
+import BaseIcon from "../components/BaseIcon.vue";
 </script>
 
 <template>
 <div class="bg-darkblue w-full flex items-center justify-center h-[100vh]">
     <div class="flex flex-col items-center max-w-112.5 w-full bg-white px-18 py-14 rounded-lg">
     <SiteLogo />
+    <BaseIcon name="UserIcon"/>
 </div>
 </div>
 </template>
