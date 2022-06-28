df9f97d6f69b825667af521c581eee518266a0ca
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-06-29 01:57:29 +0500

    feat: register page's layout has been completed

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import BaseInput from "./BaseInput.vue";
+import BaseIcon from "./BaseIcon.vue";
+import { Form } from "vee-validate";
+</script>
+
+<template>
+  <Form class="w-full">
+    <BaseInput class="mb-4" name="input" type="text" placeholder="Логин">
+      <template #append-icon>
+        <BaseIcon name="ProfileIcon" />
+      </template>
+    </BaseInput>
+
+    <BaseInput class="mb-4" name="email" type="email" placeholder="Электронная почта">
+      <template #append-icon>
+        <BaseIcon name="EmailIcon" />
+      </template>
+    </BaseInput>
+
+    <BaseInput class="mb-4" name="password" type="password" placeholder="Пароль">
+      <template #append-icon>
+        <BaseIcon name="LockIcon" />
+      </template>
+    </BaseInput>
+
+    <BaseInput class="mb-4" name="verify-password" type="password" placeholder="Повторить пароль">
+      <template #append-icon>
+        <BaseIcon name="LockIcon" />
+      </template>
+    </BaseInput>
+  </Form>
+</template>
